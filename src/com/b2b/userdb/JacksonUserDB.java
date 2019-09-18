a535e962e075bc547938a8b4b11727e600c80958
vvasireddygithub <venugopal.vasireddy@gmail.com>
2019-09-18 16:51:53 -0700

    Added array in place of collection

--- src/com/b2b/userdb/JacksonUserDB.java
+++ src/com/b2b/userdb/JacksonUserDB.java
@@ -31,37 +31,36 @@ import com.google.gson.reflect.TypeToken;
 
 public class JacksonUserDB {
 
-	//private static ArrayList<SFTPUserProfile> users = new ArrayList<>();
-	private static ConcurrentHashMap<String,SFTPUserProfile> users = new ConcurrentHashMap<>();
+	// private static ArrayList<SFTPUserProfile> users = new ArrayList<>();
+	private static ConcurrentHashMap<String, SFTPUserProfile> users = new ConcurrentHashMap<>();
 
 	private static final Logger log = LoggerFactory.getLogger(JacksonUserDB.class.getName());
-	
-	public static ConcurrentHashMap<String,SFTPUserProfile> getUserProfiles() {
+
+	public static ConcurrentHashMap<String, SFTPUserProfile> getUserProfiles() {
 		return users;
 	}
 
-	public static void addUser(SFTPUserProfile user1)
-	{
-		users.put(user1.getUserid(),user1);
+	public static void addUser(SFTPUserProfile user1) {
+		users.put(user1.getUserid(), user1);
 		persistUserProfiles();
 	}
-	
+
 	public static void addUser(String userid, String homefolder, String password, String[] pubkeys) {
-		if (userid==null || homefolder==null )
-		{
+		if (userid == null || homefolder == null) {
 			log.error("User id and homefolder cannot be null " + " userid=" + userid + " homefolder " + homefolder);
 			return;
 		}
 		SFTPUserProfile user1 = new SFTPUserProfile();
 		user1.setUserid(userid);
 		user1.setHomeFolder(homefolder);
-		if(password != null)  user1.setPassword(CryptUtl.encrypt("hello","idievarikitelustidi"));
+		if (password != null)
+			user1.setPassword(CryptUtl.encrypt("hello", "idievarikitelustidi"));
 		// String keys1[]= {"key1","key2"};
 		if (pubkeys != null && pubkeys.length > 0)
 			user1.setPublickeys(new String[] { "key1", "key2" });
-		users.put(userid,user1);
+		users.put(userid, user1);
 		persistUserProfiles();
-		
+
 	}
 
 	public static void initUserProfiles() {
@@ -69,18 +68,18 @@ public class JacksonUserDB {
 		SFTPUserProfile user1 = new SFTPUserProfile();
 		user1.setUserid("venu100");
 		user1.setHomeFolder("C:\\Users\\harip\\SFTP101");
-		user1.setPassword(CryptUtl.encrypt("hello","idievarikitelustidi"));
+		user1.setPassword(CryptUtl.encrypt("hello", "idievarikitelustidi"));
 		// String keys1[]= {"key1","key2"};
 		user1.setPublickeys(new String[] { "key1", "key2" });
-		users.put("venu100",user1);
+		users.put("venu100", user1);
 
 		SFTPUserProfile user2 = new SFTPUserProfile();
 		user2.setUserid("venu101");
 		user2.setHomeFolder("C:\\Users\\harip\\SFTP101");
-		user2.setPassword(CryptUtl.encrypt("hello43","idievarikitelustidi"));
+		user2.setPassword(CryptUtl.encrypt("hello43", "idievarikitelustidi"));
 
 		user2.setPublickeys(new String[] { "key3", "key4" });
-		users.put("venu101",user2);
+		users.put("venu101", user2);
 		persistUserProfiles();
 	}
 
@@ -93,9 +92,13 @@ public class JacksonUserDB {
 
 		try {
 			System.out.println(B2BServerPropertiesManager.get(B2BConstants.USERS_JSON, "users.json"));
+
+			Collection<SFTPUserProfile> collection = users.values();
+			SFTPUserProfile[] userArray = collection.toArray(new SFTPUserProfile[0]);
+
+			mapper.writeValue(new File(B2BServerPropertiesManager.get(B2BConstants.USERS_JSON, "users.json")),
+					userArray);
 			
-			mapper.writeValue(new File(B2BServerPropertiesManager.get(B2BConstants.USERS_JSON, "users.json")), users);
-			// mapper.
 			// mapper.updateValue(valueToUpdate, overrides)
 			// mapper.updateValue(valueToUpdate, overrides)
 
@@ -114,39 +117,43 @@ public class JacksonUserDB {
 
 		try {
 			@SuppressWarnings("unchecked")
-			ConcurrentHashMap myObjects = mapper.readValue(
+			SFTPUserProfile[] userArray = mapper.readValue(
 					new File(B2BServerPropertiesManager.get(B2BConstants.USERS_JSON, "users.json")),
-					ConcurrentHashMap.class);
+					SFTPUserProfile[].class);
+			users.clear();
+			for (SFTPUserProfile user : userArray) {
+				users.put(user.getUserid(), user);
+			}
 
-			java.util.LinkedHashMap obj1 = (LinkedHashMap) myObjects.get("venu100");
-			SFTPUserProfile s = (SFTPUserProfile)obj1.entrySet();
-			System.out.println(s.getPassword());
-			
-			//users = new ArrayList<SFTPUserProfile>(Arrays.asList(myObjects));
+			// SFTPUserProfile s = (SFTPUserProfile)obj1.entrySet();
+			// System.out.println(s.getPassword());
+
+			// users = new ArrayList<SFTPUserProfile>(Arrays.asList(myObjects));
 
 			// mapper.writerWithDefaultPrettyPrinter().writeValueAsString(myObjects);
 
 			// System.out.println(myObjects[0]);
 			// System.out.println(myObjects[1]);
-			
-			 myObjects.get("venu101");
-			
-			//System.out.println(" password " + user1.getPassword());
-			
-			//System.out.println(" password " + CryptUtl.decrypt(password, "idievarikitelustidi"));
-			
-			
-			
- 
+
+			// myObjects.get("venu101");
+
+			// System.out.println(" password " + user1.getPassword());
+
+			// System.out.println(" password " + CryptUtl.decrypt(password,
+			// "idievarikitelustidi"));
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
+		System.out.println("Users Size " + users.size());
+		System.out.println("Users Size " + users.get("venu101"));
+
 	}
 
 	public static void main(String[] args) {
-		//initUserProfiles();		
+		initUserProfiles();
 		loadUserProfiles();
 	}
 
